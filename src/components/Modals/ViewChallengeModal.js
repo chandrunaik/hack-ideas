6a9d5a09a15490d6e1d666380c7579f585501731
Chandru Naik <cmnaik007@gmail.com>
2021-11-14 17:02:57 +0530

    update props

--- src/components/Modals/ViewChallengeModal.js
+++ src/components/Modals/ViewChallengeModal.js
@@ -1,29 +1,25 @@
 import React, {useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
 
-function ViewChallengeModal(props) {
+function ViewChallengeModal({open, challenge, onclose}) {
   const modalRef = useRef();
 
   useEffect(() => {
-    if (props.open) {
+    if (open) {
       modalRef.current.showModal();
     } else {
       modalRef.current.close();
     }
-  }, [props]);
-
-  const closeModal = () => {
-    props.onclose();
-  };
+  }, [open]);
 
   return ReactDOM.createPortal(
     <dialog className="hModal" ref={modalRef}>
-      <h5 className="mb-3 title">{props.challenge.title}</h5>
-      <p className="addedByLabel">Added by: {props.challenge.createdBy}</p>
-      <p className="mb-3 description">{props.challenge.description}</p>
+      <h5 className="mb-3 title">{challenge.title}</h5>
+      <p className="addedByLabel">Added by: {challenge.createdBy}</p>
+      <p className="mb-3 description">{challenge.description}</p>
       <div className="hTags mb-3">
-        {props.challenge.tags &&
-          props.challenge.tags.map((tag) => {
+        {challenge.tags &&
+          challenge.tags.map((tag) => {
             return (
               <span className="hTag small" key={tag}>
                 {tag}
@@ -32,7 +28,7 @@ function ViewChallengeModal(props) {
           })}
       </div>
       <div className="mt-3 d-flex justify-content-end">
-        <input className="btn btn-secondary" type="button" value="Cancel" onClick={closeModal} />
+        <input className="btn btn-secondary" type="button" value="Cancel" onClick={onclose} />
         <pre> </pre>
       </div>
     </dialog>,
